304dc878f18920e80ac534dd59632c50bbc7e5a8
Siddhartha Jaiswal <j.siddhartha2003@gmail.com>
2020-06-24 20:30:09 +0530

    history and portfolio functionality completed

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 import mysql.connector
 import datetime
 
-from helpers import plot_data, get_ohlcv, login_required
+from helpers import plot_data, get_ohlcv, login_required, inr
 from news import get_news
 
 # Configure application
@@ -203,7 +203,7 @@ def watchlist():
         # if Quote was not submitted
         if not request.form.get("Quote"):
             flash(u"Must provide symbol","error")
-            return render_template("portal.html")
+            return render_template("watchlist.html")
 
         symbol = request.form.get("Quote")
 
@@ -222,16 +222,59 @@ def watchlist():
             # if incorrect symbol given
             except:
                 flash(u"Please enter valid symbol","error")
-                return render_template("portal.html")
+                return redirect("/watchlist")
             break
-            
-        symbolDict = df.to_dict('records')[0]
+        
+        # inserting new symbol into the watchlist
+        crsr.execute("INSERT INTO watchlist (symbol, userid) VALUES ('{}', {})".format(symbol, session["user_id"]))
+        mydb.commit()
 
-        return render_template("watchlist.html", symbolDict=symbolDict, symbol=symbol)
+        # getting all symbols from the watchlist table
+        crsr.execute("SELECT symbol FROM watchlist where userid = {}".format(session["user_id"]))
+        rows = crsr.fetchall()
+        
+        symbolDictList = []
+        symbolList = []
+        for row in rows:
+            df = get_ohlcv(row[0], strdate)
+            symbolDict = df.to_dict('records')[0]
+            
+            symbolDictList.append(symbolDict)
+            symbolList.append(row[0])
+
+        return render_template("watchlist.html", symbolDictList = symbolDictList, symbolList = symbolList)
     
     # else if user reached route via GET (as by clicking a link or via redirect)
     else:
-        return render_template("watchlist.html")
+        
+        # getting all symbols from the watchlist table
+        crsr.execute("SELECT symbol FROM watchlist where userid = {}".format(session["user_id"]))
+        rows = crsr.fetchall()
+         
+        # getting the latest date when we have data
+        today = datetime.date.today()
+        date = today
+        strdate = datetime.date.today().strftime("%d-%m-%Y")
+
+        while True:
+            try:
+                df = get_ohlcv('SBIN', strdate)
+            except KeyError:
+                date = date - datetime.timedelta(days=1)
+                strdate = date.strftime("%d-%m-%Y")
+                continue
+            break
+
+        symbolDictList = []
+        symbolList = []
+        for row in rows:
+            df = get_ohlcv(row[0], strdate)
+            symbolDict = df.to_dict('records')[0]
+            
+            symbolDictList.append(symbolDict)
+            symbolList.append(row[0])
+
+        return render_template("watchlist.html", symbolDictList = symbolDictList, symbolList = symbolList)
 
 
 @app.route("/news")
@@ -246,6 +289,237 @@ def currency():
     return render_template('currency.html')
     
 
+@app.route("/buy", methods=['GET', 'POST'])
+@login_required
+def buy():
+
+    # if user reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        
+        # if Quote was not submitted
+        if not request.form.get("Quote"):
+            flash(u"Must provide symbol","error")
+            return render_template("buy.html")
+
+        #if shares not submitted
+        if not request.form.get("Number"):
+            flash(u"Must provide number of shares","error")
+            return render_template("buy.html")
+
+        
+        symbol = request.form.get("Quote")
+        shares = eval(request.form.get("Number"))
+
+        #if not an integer in shares
+        if type(eval(request.form.get("Number"))) != int:
+            flash(u"Must provide a number","error")
+            return render_template("buy.html")
+
+
+
+        # getting latest data in case market is closed (for whatever reason)
+        today = datetime.date.today()
+        date = today
+        strdate = datetime.date.today().strftime("%d-%m-%Y")
+
+        while True:
+            try:
+                df = get_ohlcv(symbol, strdate)
+            except KeyError:
+                date = date - datetime.timedelta(days=1)
+                strdate = date.strftime("%d-%m-%Y")
+                continue
+            # if incorrect symbol given
+            except:
+                flash(u"Please enter valid symbol","error")
+                return render_template("buy.html")
+            break
+            
+        symbolDict = df.to_dict('records')[0]
+
+
+        #Inserting data in history table
+        crsr.execute("INSERT INTO history (Symbol, Shares, Price, Date_Time, UserId) VALUES ('{}',{}, {}, '{}', {})".format(symbol, shares, symbolDict["Close"], datetime.datetime.now(), session["user_id"] ))
+        mydb.commit()
+
+        #Inserting data in portfolio
+        crsr.execute("Select symbol from portfolio where userID = {}".format(session['user_id']))
+        symbols_present = crsr.fetchall()
+
+        existing_symbols = []
+        for i in symbols_present:
+            for j in i:
+                existing_symbols.append(j)
+
+        print(existing_symbols)
+
+        if symbol in existing_symbols:
+            crsr.execute("Update Portfolio set shares = shares + {} where userId = {} and symbol = '{}'".format(shares, session['user_id'], symbol))
+            mydb.commit()
+
+            
+        else:
+            crsr.execute("INSERT INTO portfolio(Symbol, Shares,  UserId) VALUES ('{}',{}, {})".format(symbol, shares, session["user_id"] ))
+            mydb.commit()
+
+        #Decrease Cash
+        totalprice = shares*int(symbolDict["Close"])
+        print(totalprice)
+
+        crsr.execute("Update Users Set Cash = Cash - {} where Id = {}".format(totalprice, session['user_id']))
+        mydb.commit()
+        
+
+        return render_template("buy.html", symbolDict = symbolDict, symbol = symbol, shares = shares)
+
+    # else if user reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("buy.html")
+
+@app.route("/sell", methods=['GET', 'POST'])
+@login_required
+def sell():
+
+    # if user reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        
+        # if Quote was not submitted
+        if not request.form.get("Quote"):
+            flash(u"Must provide symbol","error")
+            return render_template("sell.html")
+
+        #if shares not submitted
+        if not request.form.get("Number"):
+            flash(u"Must provide number of shares","error")
+            return render_template("sell.html")
+
+        
+        symbol = request.form.get("Quote")
+        shares = - eval(request.form.get("Number"))
+
+        #if not an integer in shares
+        if type(eval(request.form.get("Number"))) != int:
+            flash(u"Must provide a number","error")
+            return render_template("sell.html")
+
+        #if shares are not there
+        crsr.execute("Select shares from portfolio where symbol = '{}' and userId = {}",format(symbol, session["user_id"]))
+        current_shares = crsr.fetchall()
+
+        shares_owned = int()
+
+
+        #if not enough stock
+        for i in current_shares:
+            for j in i:
+                shares_owned = int(j)
+                
+        if eval(request.form.get("Number")) > shares_owned:
+            flash(u"Not enough shares","error")
+            return render_template("sell.html")
+
+
+
+        # getting latest data in case market is closed (for whatever reason)
+        today = datetime.date.today()
+        date = today
+        strdate = datetime.date.today().strftime("%d-%m-%Y")
+
+        while True:
+            try:
+                df = get_ohlcv(symbol, strdate)
+            except KeyError:
+                date = date - datetime.timedelta(days=1)
+                strdate = date.strftime("%d-%m-%Y")
+                continue
+            # if incorrect symbol given
+            except:
+                flash(u"Please enter valid symbol","error")
+                return render_template("sell.html")
+            break
+            
+        symbolDict = df.to_dict('records')[0]
+
+
+        #Inserting data in history table
+        crsr.execute("INSERT INTO history (Symbol, Shares, Price, Date_Time, UserId) VALUES ('{}',{}, {}, '{}', {})".format(symbol, shares, symbolDict["Close"], datetime.datetime.now(), session["user_id"] ))
+        mydb.commit()
+
+        #Inserting data in portfolio
+        crsr.execute("Select symbol from portfolio where userID = {}".format(session['user_id']))
+        symbols_present = crsr.fetchall()
+
+        existing_symbols = []
+        for i in symbols_present:
+            for j in i:
+                existing_symbols.append(j)
+
+        print(existing_symbols)
+
+        if symbol in existing_symbols:
+            crsr.execute("Update Portfolio set shares = shares + {} where userId = {} and symbol = '{}'".format(shares, session['user_id'], symbol))
+            mydb.commit()
+        
+        
+        return render_template("sell.html", symbolDict = symbolDict, symbol = symbol, shares = shares)
+
+    # else if user reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("sell.html")
+
+
+@app.route("/history")
+def history():
+    """ Show transaction history """
+    crsr.execute("SELECT symbol, shares, price, date_time FROM history WHERE userid = {} ORDER BY date_time DESC".format(session["user_id"]))
+    rows = crsr.fetchall()
+
+    return render_template("history.html", rows=rows)
+
+@app.route("/portfolio")
+@login_required
+def portfolio():
+    """Show portfolio of stocks"""
+
+    crsr.execute("SELECT cash FROM users WHERE id = {}".format(session["user_id"]))
+    rows = crsr.fetchall()
+    cash = rows[0][0]
+
+    crsr.execute("SELECT symbol, shares FROM portfolio WHERE userid = {}".format(session["user_id"]))
+    rows = crsr.fetchall()
+
+    # getting latest data in case market is closed (for whatever reason)
+    today = datetime.date.today()
+    date = today
+    strdate = datetime.date.today().strftime("%d-%m-%Y")
+
+    costList = []
+    priceList = []
+    pricetotal = 0
+
+    for row in rows:
+        crsr.execute("SELECT SUM(price * shares) FROM history WHERE userid = {} and symbol = '{}'".format(session["user_id"], row[0]))
+        cost = crsr.fetchall()
+        costList.append(int(cost[0][0]))
+
+        while True:
+            try:
+                df = get_ohlcv(row[0], strdate)
+            except KeyError:
+                date = date - datetime.timedelta(days=1)
+                strdate = date.strftime("%d-%m-%Y")
+                continue
+            break
+        currentPrice = df.to_dict('records')[0]['Close']
+        priceList.append(currentPrice)
+
+        pricetotal = currentPrice * row[1] + pricetotal 
+
+    grandtotal = cash + pricetotal
+    grandtotal = inr(grandtotal)
+    cash = inr(cash)
+
+    return render_template("portfolio.html", grandtotal = grandtotal, cash = cash, rows = rows, priceList = priceList, costList = costList)
 
 
 if __name__ == '__main__':
